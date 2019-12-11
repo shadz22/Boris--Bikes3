98781f8767180f4dfb128b01ca61a3fc5006f837
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2019-12-11 16:52:31 +0000

    Added the set for returning docked bikes, added the :bike attribute reader

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,10 +1,12 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_reader :bike
   def release_bike
     bike = ""
     bike = Bike.new
   end
-  def dock 
+  def dock(bike)
+    @bike = bike
   end
 end 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,8 @@
 require 'docking_station'
 
 describe DockingStation do 
+
+  
   it { is_expected.to respond_to :release_bike }
   
 
@@ -8,6 +10,19 @@ describe DockingStation do
     bike = subject.release_bike
     expect(bike).to be_working
   end 
+
   it { is_expected.to respond_to :dock }
+  it { is_expected.to respond_to(:dock).with(1).argument } 
+
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end 
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end 
 
 end 
