7518223bab51708b91af329566935c01a2e83970
shadz22 <shadz22@yahoo.co.uk>
2019-12-12 16:51:35 +0000

    Putting bikes in an array

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,13 +1,17 @@
 require_relative 'bike'
 
 class DockingStation
-  attr_reader :bike
+  def initialize
+    @bikes = []
+  end
+
   def dock(bike)
-    fail "Docking station full" if @bike
-    @bike = bike
+    fail "Docking station full" if @bikes.count >= 20
+    @bikes << bike
   end
+
   def release_bike
-    fail "No bikes available" unless @bike
-    @bike 
+    fail "No bikes available" if @bikes.empty?
+    @bikes.pop
   end
-end 
+end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,10 @@
 require 'docking_station'
 
-describe DockingStation do 
+describe DockingStation do
 
-  
+
   it { is_expected.to respond_to :release_bike }
-  
+
   describe '#release_bike' do
 
     it 'releases working bikes' do
@@ -12,7 +12,7 @@ describe DockingStation do
       subject.dock(bike)
       bike = subject.release_bike
       expect(bike).to be_working
-    end 
+    end
 
     it 'releases a bike' do
       bike = Bike.new
@@ -27,22 +27,22 @@ describe DockingStation do
 
     describe '#dock' do
 
-       it { is_expected.to respond_to(:dock).with(1).argument } 
+       it { is_expected.to respond_to(:dock).with(1).argument }
 
       it 'docks something' do
       bike = Bike.new
-      expect(subject.dock(bike)).to eq bike
-      end 
+      expect(subject.dock(bike)).to eq [bike]
+      end
 
       it 'returns docked bikes' do
       bike = Bike.new
-      subject.dock(bike)
-      expect(subject.bike).to eq bike
-      end 
+      #subject.dock(bike)
+      expect(subject.dock(bike)).to eq [bike]
+      end
 
       it 'raises an error when the docking station is full' do
-        subject.dock(Bike.new)
+        20.times {subject.dock(Bike.new)}
         expect { subject.dock(Bike.new) }.to raise_error "Docking station full"
-      end 
-    end 
-end 
+      end
+    end
+end
